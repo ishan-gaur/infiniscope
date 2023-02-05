f3f07206cc2336f13b8cc2948c5f8ca319f9cca7
Ishan Gaur <ishangaur4@gmail.com>
2023-02-05 23:52:41 +0100

    cleaned up delirium code from last night jam sess. intensity histograms, histograms of class biases in data, logfile with common localizations, proteins, and cell-lines; distributions of multilocalization.

--- dataset_explorer.py
+++ dataset_explorer.py
@@ -1,3 +1,4 @@
+# TODO these need to be abstracted so they can be used over model outputs at inferece time
 # TODO use the streaming module instead of loading into memory?
 
 import os
@@ -15,6 +16,7 @@ from pytorch_lightning.trainer import Trainer
 
 from main import get_parser, instantiate_from_config
 
+
 now = datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
 
 # add cwd for convenience
@@ -25,6 +27,7 @@ parser = Trainer.add_argparse_args(parser)
 
 opt, unknown = parser.parse_known_args()
 
+# setup logging environment
 if opt.name:
     name = "_" + opt.name
 elif opt.base:
@@ -33,6 +36,7 @@ elif opt.base:
     name = "_" + cfg_name
 else:
     name = ""
+
 nowname = now + name + opt.postfix
 logdir = os.path.join(opt.logdir, nowname)
 
@@ -44,6 +48,14 @@ seed_everything(opt.seed)
 configs = [OmegaConf.load(cfg) for cfg in opt.base]
 cli = OmegaConf.from_dotlist(unknown)
 config = OmegaConf.merge(*configs, cli)
+
+# setup logger to send output to file
+# TODO: change logdir name to include dataset name
+logdir = os.path.join(opt.logdir, f"dataset_{config.data['name']}_{now}")
+log_file = os.path.join(logdir, f"dataset_explorer.log")
+os.makedirs(os.path.dirname(log_file))
+print(f"Saving logs to {log_file}")
+sys.stdout = open(log_file, "w")
 
 
 # Retrieve dataset from config
@@ -59,17 +71,20 @@ data_profile = {}
 start_index = 0
 for k in data.datasets:
     print(f"{k}, {data.datasets[k].__class__.__name__}, {len(data.datasets[k])}")
-    # dataset = data.datasets[k].samples
+    
+    # TODO: All the samples in data are in a single array
     offset = len(data.datasets[k])
-    dataset = data.datasets[k].samples[start_index : start_index + offset] # TODO: All the samples in data are in a single array
+    dataset = data.datasets[k].samples[start_index : start_index + offset] 
     start_index += offset
     
+    # data_profile is an array of dictionaries, one for each dataset
     data_profile[k] = {}
     proteins, cell_lines, locs = Counter(), Counter(), Counter()
 
     for sample in dataset:
         sample_type = sample['caption'].split('/')
         sample_type[2] = sample_type[2].split(',')
+        # TODO: check that this isn't actually used
         if sample_type[0] == 'nan':
             continue
         proteins.update([sample_type[0]])
@@ -80,6 +95,7 @@ for k in data.datasets:
     data_profile[k]["Cell Line Counts"] = cell_lines
     data_profile[k]["Localization Counts"] = locs
 
+    # plot and save data
     fig, axs = plt.subplots(1, len(data_profile[k]))
     for i, label in enumerate(data_profile[k].keys()):
         x = data_profile[k][label].values()
@@ -87,17 +103,17 @@ for k in data.datasets:
         axs[i].hist(x, bins=10, density=True)
         axs[i].title.set_text(label)
 
-        with open(f"{k}_{label}.csv", "w", newline="") as f:
+        with open(os.path.join(logdir, f"{k}_{label}.csv"), "w", newline="") as f:
             fieldnames = [label, "count"]
             writer = csv.writer(f)
             writer.writerow(fieldnames)
             for key, val in data_profile[k][label].items():
                 writer.writerow([key, val])   
 
-    plt.savefig(f'{k}.png')
+    plt.savefig(os.path.join(logdir, f'{k}.png'))
 
 # calculate counts of common proteins, cell-lines, and localizations between datasets
-# copy data
+# for each dataset, get the intersection of proteins, cell-lines, and localizations
 common = copy.deepcopy(data_profile[k])
 for k in data_profile.keys():
     for c in common:
@@ -105,9 +121,10 @@ for k in data_profile.keys():
 
 for c in common:
     for v in common[c]:
-        # denominator should be updated to exclude nans
+        # TODO: denominator should be updated to exclude nans
         common[c][v] = [common[c][v] / len(data.datasets[k]) for k in data_profile.keys()]
 
+# convert counts to percentages of each dataset
 for label in common:
     for k, counts in common[label].items():
         common[label][k] = [str(round(100 * count, 1)) + "%" for count in counts]
@@ -139,7 +156,7 @@ plt.clf()
 x = protein_localization_counts.values()
 plt.hist(x, bins=10)
 plt.title("Protein Localization Counts")
-plt.savefig('protein_localization_counts.png')
+plt.savefig(os.path.join(logdir, 'protein_localization_counts.png'))
 
 # print protein localization counts by dataset
 for k in data_profile:
@@ -170,4 +187,4 @@ for i, k in enumerate(data.datasets):
     axs[1][i].hist(var_intensities, bins=10, density=True)
     # axs[1][i].title.set_text(f"{k} Intensity Var Histogram")
 
-plt.savefig('intensity_histograms.png')
+plt.savefig(os.path.join(logdir, 'intensity_histograms.png'))
