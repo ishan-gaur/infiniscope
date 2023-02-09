4355c7a1bdbcc955bbc66e0686f64eab7276d07f
Ishan Gaur <ishangaur4@gmail.com>
2023-02-09 10:26:24 +0100

    Refactored first half of the script into classes.

--- dataset_explorer.py
+++ dataset_explorer.py
@@ -1,5 +1,6 @@
 # TODO rerun analysis from calculated stats on existing folder
     # Need to save indices of each dataset for thi
+# TODO timestamp data_caches so that the Explorer class can lazily regenerate stuff
 # TODO these need to be abstracted so they can be used over model outputs at inferece time
 # TODO and so that they all just deal with the dataset structure or external defined datatypes
 # TODO use the streaming module instead of loading into memory?
@@ -13,8 +14,6 @@ import os
 import sys
 import csv
 import copy
-import shutil
-import datetime
 import itertools
 import numpy as np
 import prettyprinter as pp
@@ -23,206 +22,167 @@ import matplotlib.pyplot as plt
 from scipy.special import kl_div
 from pytorch_lightning.trainer import Trainer
 
-from main import instantiate_from_config
-from dataset_utils import load_config, printTab
+from dataset_utils import DatasetExplorer, DataProfile
+from dataset_utils import load_config, allocate_logdir, printTab
 
-config, opt, log_dir = load_config()
 
-# setup logger to send output to file
-if opt.dev:
-    logdir = os.path.join(opt.logdir, "dataset_explorer_dev")
-else:
-    now = datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
-    logdir = os.path.join(opt.logdir, f"dataset_{config.data['name']}_{now}")
+config, opt = load_config()
+logdir = allocate_logdir(config, opt)
+
 log_file = os.path.join(logdir, f"dataset_explorer.log")
-os.makedirs(os.path.dirname(log_file), exist_ok=opt.dev)
-if opt.dev:
-    for root, dirs, files in os.walk(logdir):
-        for f in files:
-            os.unlink(os.path.join(root, f))
-        for d in dirs:
-            shutil.rmtree(os.path.join(root, d))
-
 print(f"Saving logs to {log_file}")
 sys.stdout = open(log_file, "w")
 
+data_exp = DatasetExplorer(config, logdir)
 
-# Retrieve dataset from config
-# NOTE according to https://pytorch-lightning.readthedocs.io/en/latest/datamodules.html
-# calling these ourselves should not be necessary but it is.
-# lightning still takes care of proper multiprocessing though
-data = instantiate_from_config(config.data)
-data.prepare_data()
-data.setup()
+# See reference of DataProfile class for more details
+# We need to pass in a list of tuples, where each tuple is a label 
+# and a function that extracts the feature from a sample and returns
+# a list of values to add to the counter.
+def get_protein(sample):
+    return [sample['caption'].split('/')[0]]
+
+def get_cell_line(sample):
+    return [sample['caption'].split('/')[1]]
+
+def get_location(sample):
+    return sample['caption'].split('/')[2].split(',')
+
+PROT, CELL, LOC = "Protein", "Cell Line", "Location"
+profile_features = [
+    (PROT, get_protein),
+    (CELL, get_cell_line),
+    (LOC, get_location),
+]
 
 # Retrieve protein, cell_line, and location counts
-data_profile = {}
-start_index = 0
-for k in data.datasets:
-    print(f"\n{k}, {data.datasets[k].__class__.__name__}, {len(data.datasets[k])}")
+profiles = data_exp.get_profile(profile_features)
+for dataset, profile in profiles.items():
+    print(f"\nDataset: {dataset}")
+    for feature, _ in profile_features:
+        printTab(f"Top 5 {feature}s: {profile.top_percents(feature)}")
+
+    profile.plot_profile(profile_features, dataset)
+    profile.write_to_csv(profile_features, dataset)
+
+
+# # calculate counts of common proteins, cell-lines, and localizations between datasets
+# # for each dataset, get the intersection of proteins, cell-lines, and localizations
+# common = copy.deepcopy(data_profile[dataset_name])
+# total = {_k: set(v.keys()) for _k, v in data_profile[dataset_name].items()}
+# for k in data_profile.keys(): # for each dataset
+#     for c in common: # count of proteins, cell-lines, and localizations
+#         common[c] = common[c] & data_profile[k][c]
+#         total[c] = total[c] | set(data_profile[k][c].keys())
+
+# for c in common:
+#     prop_shared = len(common[c]) / len(total[c])
+#     print(f"{common[c]} of {total[c]} classes in {c}, or {str(round(100 * prop_shared, 2))}: {len(common[c])}")
+#     for v in common[c]:
+#         # TODO: denominator should be updated to exclude nans
+#         common[c][v] = [common[c][v] / len(data.datasets[k]) for k in data_profile.keys()]
+
+# # convert counts to percentages of each dataset
+# for label in common:
+#     for k, counts in common[label].items():
+#         common[label][k] = [str(round(100 * count, 1)) + "%" for count in counts]
+
+# pp.pprint(common)
+
+# # report KL-divergence between attribute distributions
+# # need ps to get list across all keys, not just those present in one dataset or the other
+# div_smoothing = {attr: [] for attr in total}
+# smoothing_range = [0.1 ** i for i in range(0, 10)]
+# for smoothing in smoothing_range:
+#     ps = {k: {attr: [] for attr in data_profile[k]} for k in data_profile}
+#     # todo sep KL by attribute
+#     for k in data.datasets:
+#         for attribute in total:
+#             for classname in total[attribute]:
+#                 ps[k][attribute].append(data_profile[k][attribute][classname])
+#             ps[k][attribute] = np.asarray(ps[k][attribute]) + len(data.datasets[k]) * smoothing
+#             norm = np.sum(ps[k][attribute])
+#             ps[k][attribute] /= norm 
+
+#     combos = list(itertools.combinations(data.datasets.keys(), 2))
+#     for combo in combos:
+#         if smoothing == smoothing_range[2]: # turns out the exponent op on floats is imprecise
+#             print(f"KL-divergence per feature for {combo[1]} and {combo[0]}:")
+#         for attribute in total: # TODO: save these key lists into specially named things
+#             div = np.sum(kl_div(ps[combo[1]][attribute], ps[combo[0]][attribute]))
+#             div_smoothing[attribute].append(div) # only while two datasets
+#             if smoothing == smoothing_range[2]:
+#                 print(f"\t{attribute}: {div}")
+
+# # plot line graph of KL-divergence per feature as smoothing varies
+# plt.clf()
+# fig, axs = plt.subplots(1, len(div_smoothing))
+# fig.suptitle('KL-divergence per feature as smoothing varies')
+# for i, attribute in enumerate(div_smoothing):
+#     axs[i].set_title(attribute)
+#     axs[i].plot(smoothing_range, div_smoothing[attribute])
+#     axs[i].set_xscale('log')
+# plt.savefig(os.path.join(logdir, f'kl_divergence.png'))
+
+
+# # get the localizations of each protein
+# protein_localizations = {}
+# start_index = 0
+# for k in data.datasets:
+#     offset = len(data.datasets[k])
+#     dataset = data.datasets[k].samples[start_index : start_index + offset] # TODO: All the samples in data are in a single array
+#     start_index += offset
     
-    # TODO: All the samples in data are in a single array
-    offset = len(data.datasets[k])
-    dataset = data.datasets[k].samples[start_index : start_index + offset] 
-    start_index += offset
-    
-    # data_profile is an array of dictionaries, one for each dataset
-    data_profile[k] = {}
-    proteins, cell_lines, locs = Counter(), Counter(), Counter()
+#     for sample in dataset:
+#         sample_type = sample['caption'].split('/')
+#         sample_type[2] = sample_type[2].split(',')
+#         if sample_type[0] not in protein_localizations:
+#             protein_localizations[sample_type[0]] = set()
+#         protein_localizations[sample_type[0]].update(sample_type[2])
 
-    for sample in dataset:
-        sample_type = sample['caption'].split('/')
-        sample_type[2] = sample_type[2].split(',')
-        # TODO: check that this isn't actually used
-        if sample_type[0] == 'nan':
-            continue
-        proteins.update([sample_type[0]])
-        cell_lines.update([sample_type[1]])
-        locs.update(sample_type[2])
+# # count the number of localizations for each protein
+# protein_localization_counts = {}
+# for protein, localizations in protein_localizations.items():
+#     protein_localization_counts[protein] = len(localizations)
 
-    data_profile[k]["Protein Counts"] = proteins
-    data_profile[k]["Cell Line Counts"] = cell_lines
-    data_profile[k]["Localization Counts"] = locs
+# # plot counts of localizations for each protein
+# plt.clf()
+# x = protein_localization_counts.values()
+# plt.hist(x, bins=10)
+# plt.title("Protein Localization Counts")
+# plt.savefig(os.path.join(logdir, 'protein_localization_counts.png'))
 
-    make_percent = lambda counts: {k: f"{round(100 * v / len(dataset), 2)}%" for (k, v) in counts}
-    printTab(f"Top 5 Proteins: {make_percent(proteins.most_common(5))}")
-    printTab(f"Top 5 Cell Lines: {make_percent(cell_lines.most_common(5))}")
-    printTab(f"Top 5 Locations: {make_percent(locs.most_common(5))}")
+# # print protein localization counts by dataset
+# for k in data_profile:
+#     multilocalizing = 0
+#     for protein in data_profile[k]["Protein Counts"]:
+#         if protein_localization_counts[protein] > 1:
+#             multilocalizing += 1
+#     print(f"{k} total proteins {len(data_profile[k]['Protein Counts'])}")
+#     print(f"{k} has {multilocalizing} multilocalizing proteins")
 
-    # plot and save data
-    fig, axs = plt.subplots(1, len(data_profile[k]))
-    for i, label in enumerate(data_profile[k].keys()):
-        x = data_profile[k][label].values()
-        logbins = np.logspace(np.log(min(x)), np.log(max(x)), 10)
-        axs[i].hist(x, bins=10, density=True)
-        axs[i].title.set_text(label)
+# # histograms of dataset image intensities
+# def median(x): return np.median(x)
+# def mean(x): return np.mean(x)
+# def var(x): return np.var(x)
+# stats = [median, mean, var]
+# for stat in stats:
+#     plt.clf()
+#     fig, axs = plt.subplots(1, len(data.datasets), sharex=True, sharey=True)
+#     fig.suptitle(f'{stat.__name__} Intensity Histograms (PMF)')
+#     start_index = 0
+#     for i, k in enumerate(data.datasets):
+#         offset = len(data.datasets[k])
+#         dataset = data.datasets[k].samples[start_index : start_index + offset] # TODO
+#         start_index += offset
 
-        with open(os.path.join(logdir, f"{k}_{label}.csv"), "w", newline="") as f:
-            fieldnames = [label, "count"]
-            writer = csv.writer(f)
-            writer.writerow(fieldnames)
-            for key, val in data_profile[k][label].items():
-                writer.writerow([key, val])   
+#         intensities = []
+#         for sample in dataset:
+#             intensities.append(stat(sample['image']))
 
-    plt.savefig(os.path.join(logdir, f'{k}.png'))
+#         intensities = np.asarray(intensities)
+#         intensities = (intensities + 1) / 2 # normalize to [0, 1]
+#         axs[i].hist(intensities, bins=10, density=True)
+#         axs[i].title.set_text(k)
 
-# calculate counts of common proteins, cell-lines, and localizations between datasets
-# for each dataset, get the intersection of proteins, cell-lines, and localizations
-common = copy.deepcopy(data_profile[k])
-total = {_k: set(v.keys()) for _k, v in data_profile[k].items()}
-for k in data_profile.keys(): # for each dataset
-    for c in common: # count of proteins, cell-lines, and localizations
-        common[c] = common[c] & data_profile[k][c]
-        total[c] = total[c] | set(data_profile[k][c].keys())
-
-for c in common:
-    prop_shared = len(common[c]) / len(total[c])
-    print(f"{common[c]} of {total[c]} classes in {c}, or {str(round(100 * prop_shared, 2))}: {len(common[c])}")
-    for v in common[c]:
-        # TODO: denominator should be updated to exclude nans
-        common[c][v] = [common[c][v] / len(data.datasets[k]) for k in data_profile.keys()]
-
-# convert counts to percentages of each dataset
-for label in common:
-    for k, counts in common[label].items():
-        common[label][k] = [str(round(100 * count, 1)) + "%" for count in counts]
-
-pp.pprint(common)
-
-# report KL-divergence between attribute distributions
-# need ps to get list across all keys, not just those present in one dataset or the other
-div_smoothing = {attr: [] for attr in total}
-smoothing_range = [0.1 ** i for i in range(0, 10)]
-for smoothing in smoothing_range:
-    ps = {k: {attr: [] for attr in data_profile[k]} for k in data_profile}
-    # todo sep KL by attribute
-    for k in data.datasets:
-        for attribute in total:
-            for classname in total[attribute]:
-                ps[k][attribute].append(data_profile[k][attribute][classname])
-            ps[k][attribute] = np.asarray(ps[k][attribute]) + len(data.datasets[k]) * smoothing
-            norm = np.sum(ps[k][attribute])
-            ps[k][attribute] /= norm 
-
-    combos = list(itertools.combinations(data.datasets.keys(), 2))
-    for combo in combos:
-        if smoothing == smoothing_range[2]: # turns out the exponent op on floats is imprecise
-            print(f"KL-divergence per feature for {combo[1]} and {combo[0]}:")
-        for attribute in total: # TODO: save these key lists into specially named things
-            div = np.sum(kl_div(ps[combo[1]][attribute], ps[combo[0]][attribute]))
-            div_smoothing[attribute].append(div) # only while two datasets
-            if smoothing == smoothing_range[2]:
-                print(f"\t{attribute}: {div}")
-
-# plot line graph of KL-divergence per feature as smoothing varies
-plt.clf()
-fig, axs = plt.subplots(1, len(div_smoothing))
-fig.suptitle('KL-divergence per feature as smoothing varies')
-for i, attribute in enumerate(div_smoothing):
-    axs[i].set_title(attribute)
-    axs[i].plot(smoothing_range, div_smoothing[attribute])
-    axs[i].set_xscale('log')
-plt.savefig(os.path.join(logdir, f'kl_divergence.png'))
-
-
-# get the localizations of each protein
-protein_localizations = {}
-start_index = 0
-for k in data.datasets:
-    offset = len(data.datasets[k])
-    dataset = data.datasets[k].samples[start_index : start_index + offset] # TODO: All the samples in data are in a single array
-    start_index += offset
-    
-    for sample in dataset:
-        sample_type = sample['caption'].split('/')
-        sample_type[2] = sample_type[2].split(',')
-        if sample_type[0] not in protein_localizations:
-            protein_localizations[sample_type[0]] = set()
-        protein_localizations[sample_type[0]].update(sample_type[2])
-
-# count the number of localizations for each protein
-protein_localization_counts = {}
-for protein, localizations in protein_localizations.items():
-    protein_localization_counts[protein] = len(localizations)
-
-# plot counts of localizations for each protein
-plt.clf()
-x = protein_localization_counts.values()
-plt.hist(x, bins=10)
-plt.title("Protein Localization Counts")
-plt.savefig(os.path.join(logdir, 'protein_localization_counts.png'))
-
-# print protein localization counts by dataset
-for k in data_profile:
-    multilocalizing = 0
-    for protein in data_profile[k]["Protein Counts"]:
-        if protein_localization_counts[protein] > 1:
-            multilocalizing += 1
-    print(f"{k} total proteins {len(data_profile[k]['Protein Counts'])}")
-    print(f"{k} has {multilocalizing} multilocalizing proteins")
-
-# histograms of dataset image intensities
-def median(x): return np.median(x)
-def mean(x): return np.mean(x)
-def var(x): return np.var(x)
-stats = [median, mean, var]
-for stat in stats:
-    plt.clf()
-    fig, axs = plt.subplots(1, len(data.datasets), sharex=True, sharey=True)
-    fig.suptitle(f'{stat.__name__} Intensity Histograms (PMF)')
-    start_index = 0
-    for i, k in enumerate(data.datasets):
-        offset = len(data.datasets[k])
-        dataset = data.datasets[k].samples[start_index : start_index + offset] # TODO
-        start_index += offset
-
-        intensities = []
-        for sample in dataset:
-            intensities.append(stat(sample['image']))
-
-        intensities = np.asarray(intensities)
-        intensities = (intensities + 1) / 2 # normalize to [0, 1]
-        axs[i].hist(intensities, bins=10, density=True)
-        axs[i].title.set_text(k)
-
-    plt.savefig(os.path.join(logdir, f'{stat.__name__}_intensity_histograms.png'))
+#     plt.savefig(os.path.join(logdir, f'{stat.__name__}_intensity_histograms.png'))
--- dataset_utils.py
+++ dataset_utils.py
@@ -18,8 +18,6 @@ from main import get_parser, instantiate_from_config
 
 
 def load_config():
-    now = datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
-
     # add cwd for convenience
     sys.path.append(os.getcwd())
 
@@ -27,6 +25,18 @@ def load_config():
     parser = Trainer.add_argparse_args(parser)
 
     opt, unknown = parser.parse_known_args()
+
+    seed_everything(opt.seed)
+
+    configs = [OmegaConf.load(cfg) for cfg in opt.base]
+    cli = OmegaConf.from_dotlist(unknown)
+    config = OmegaConf.merge(*configs, cli)
+
+    return config, opt
+
+
+def allocate_logdir(config, opt):
+    config, opt = load_config()
 
     # setup logging environment
     if opt.name:
@@ -38,17 +48,124 @@ def load_config():
     else:
         name = ""
 
+    now = datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
     nowname = now + name + opt.postfix
     logdir = os.path.join(opt.logdir, nowname)
+    # setup logger to send output to file
+    if opt.dev:
+        logdir = os.path.join(opt.logdir, "dataset_explorer_dev")
+    else:
+        now = datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+        logdir = os.path.join(opt.logdir, f"dataset_{config.data['name']}_{now}")
 
-    seed_everything(opt.seed)
+    os.makedirs(os.path.dirname(logdir), exist_ok=opt.dev)
+    if opt.dev:
+        for root, dirs, files in os.walk(logdir):
+            for f in files:
+                os.unlink(os.path.join(root, f))
+            for d in dirs:
+                shutil.rmtree(os.path.join(root, d))
 
-    configs = [OmegaConf.load(cfg) for cfg in opt.base]
-    cli = OmegaConf.from_dotlist(unknown)
-    config = OmegaConf.merge(*configs, cli)
-
-    return config, opt, logdir
+    return logdir
 
 def printTab(*args):
     args = ("\t",)+args
     print(*args)
+
+
+class DatasetExplorer:
+    def __init__(self, config, logdir):
+        # Retrieve dataset from config
+        # NOTE according to https://pytorch-lightning.readthedocs.io/en/latest/datamodules.html
+        # calling these ourselves should not be necessary but it is.
+        # lightning still takes care of proper multiprocessing though
+        self.data = instantiate_from_config(config.data)
+        self.data.prepare_data()
+        self.data.setup()
+        self.logdir = logdir
+    
+    def __iter__(self):
+        return DatasetIterator(self.data.datasets)
+
+    def dataset_names(self):
+        return list(self.data.datasets.keys())
+
+    def get_profile(self, features):
+        self.profiles = {}
+        for dataset_name, samples in self:
+            class_name = self.data.datasets[dataset_name].__class__.__name__
+            print(f"{dataset_name}, {class_name}, {len(samples)}")
+            self.profiles[dataset_name] = DataProfile(samples, features, self.logdir)
+        return self.profiles
+
+class DatasetIterator:
+    def __init__(self, datasets):
+        self.datasets = datasets
+        self.i, self.keys = 0, list(datasets.keys())
+        self.offset = 0
+
+    # iterator that goes through each dataset in data
+    # the samples list is a list of samples in the dataset
+    # so if training has 10 samples and validation has 15
+    # then validation starts at data.dataset[10]
+    def __next__(self):
+        if self.i == len(self.keys):
+            raise StopIteration
+        dataset_name = self.keys[self.i]
+        dataset = self.datasets[dataset_name]
+
+        n = len(dataset)
+        assert len(dataset.samples) >= self.offset + n
+        samples = dataset.samples[self.offset:self.offset + n]
+
+        self.i += 1
+        self.offset += n
+
+        return dataset_name, samples
+
+class DataProfile:
+    # features at a label for that statistic and a function that takes
+    # a list of samples and returns that statistic
+    def __init__(self, samples, features, logdir):
+        self.prof = {}
+        for feature_name, get_feature in features:
+            self.prof[feature_name] = DataProfile.get_counts(get_feature, samples)
+        self.logdir = logdir
+    
+    def get_counts(extract_func, samples):
+        counts = Counter()
+        for sample in samples:
+            counts.update(extract_func(sample))
+        return counts
+
+    def top_percents(self, name, top=5, digits=2):
+        counts = self.prof[name]
+        total = 0
+        for k in counts:
+            total += counts[k]
+        
+        percents = {}
+        for k, v in counts.most_common(top):
+            percents[k] = f"{round(100 * v / total, digits)}%"
+        return percents
+
+    def write_to_csv(self, features, fileprefix):
+        for feature, _ in features:
+            with open(os.path.join(self.logdir, f"{fileprefix}_{feature}.csv"), "w", newline="") as f:
+                fieldnames = [feature, "count"]
+                writer = csv.writer(f)
+                writer.writerow(fieldnames)
+                for key, val in self.prof[feature].items():
+                    writer.writerow([key, val])   
+
+    # passing features manually most places so it is forwards compatibile
+    # if we don't care about some of the features later on
+    def plot_profile(self, features, filename):
+        plt.clf()
+        fig, axs = plt.subplots(1, len(features))
+        fig.suptitle('Histograms of feature categories')
+        for i, (feature, _) in enumerate(features):
+            counts = self.prof[feature].values()
+            axs[i].hist(counts, bins=10, density=True)
+            axs[i].title.set_text(feature)
+        plt.savefig(os.path.join(self.logdir, f'{filename}.png'))
